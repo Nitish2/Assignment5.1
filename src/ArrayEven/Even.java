b8d00f38acd03b042e09b479268971040c88a6b9
Nitish2 <nitishn886@gmail.com>
2017-06-08 12:31:18 +0530

    Assignment5.1

---
+++ src/ArrayEven/Even.java
@@ -0,0 +1,24 @@
+package ArrayEven;
+
+public class Even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] IntArray= new int[10];
+		int i=1;
+		System.out.println("Even numbers from 1 to 10 are:");
+		
+		for(i=1;i<IntArray.length;i++)
+		 { 
+			 if(i%2==0)
+			 {
+ 			   System.out.println(i);
+			 }
+		 
+			 }
+				 
+		
+
+	} 
+
+}
